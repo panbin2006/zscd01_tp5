60f633140b24fd4104627ae002a9ae028a1afa1e
panbin2006 <502752983@qq.com>
2018-12-22 16:08:58 +0800

    msaleday增加exportDay方法，导出销售日报表

--- application/index/controller/Msaleday.php
+++ application/index/controller/Msaleday.php
@@ -23,6 +23,7 @@ class Msaleday extends Controller
     }
 
 
+
 	/**
     **导出Excel
     **
@@ -93,6 +94,104 @@ class Msaleday extends Controller
         $objWriter->save('php://output');
 
     }
+    /**
+    **导出日报表
+    **
+    **/
+    public function exportDay($pdate){
+        
+        
+        $dir=dirname(__FILE__); 
+        //导出excel实现 、
+        $objPHPExcel = new PHPExcel(); //创建excel对象
+        $objSheet = $objPHPExcel->getActiveSheet(); //获取活动sheet
+        //设置单元格样式
+        $objSheet->getDefaultStyle()->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER);//设置单元格默认对齐方式
+        $objSheet->getDefaultStyle()->getAlignment()->setWrapText(true); //设置默认自动换行
+        $objSheet->getColumnDimension('B')->setWidth(30);//设置“施工单位”列宽
+        $objSheet->getColumnDimension('C')->setWidth(30);//设置“工程名称”列宽
+        $objSheet->getColumnDimension('D')->setWidth(30);//设置“部位”列宽
+        //第一行数据 
+        $objSheet->setTitle('日报表');
+        $objSheet->setCellValue('D1','东莞市协同混凝土有限公司'); //报表抬头，公司名称
+        $objSheet->setCellValue('D2','日 报 表'); //报表标题
+        $objSheet->getStyle('D2')->getFont()->setSize(30)->setBold(true);
+        $objSheet->setCellValue('B3','销售（方）');
+        $objSheet->setCellValue('H3',$pdate);
+        $objSheet->setCellValue('D1','东莞市协同混凝土有限公司');
+        $objSheet->setCellValue('A4','序号');
+        $objSheet->setCellValue('B4','施工单位');
+        $objSheet->setCellValue('C4','工程名称');
+        $objSheet->setCellValue('D4','工程部位');
+        $objSheet->setCellValue('E4','强度');
+        $objSheet->setCellValue('F4','浇注方式');
+        $objSheet->setCellValue('G4','数量');
+        $objSheet->setCellValue('H4','备注');
+        $objSheet->getStyle('A4:H4')->getFont()->setSize(14)->setBold(true);//设置表头字体
+        /**
+        **询数据库，填充表格 
+        **/
+        //查询所有工程名称
+        $sql="select distinct(custname) from MSaleDay where pdate='".$pdate."'"; //查询当日有生产的所有客户
+        $rows = Db::query($sql);
+        echo $sql;
+        // var_dump($rows);
+        // exit;
+        $index = 5;//填充数据首行行号
+        $num = 1;  //序号
+        foreach ($rows as $key => $val) {//循环遍历客户
+            // var_dump($val['custname']);
+            $objSheet->setCellValue('A'.$index, $num);//写入序号
+            $objSheet->setCellValue('B'.$index, $val['custname']);//写入客户
+            $old_index = $index; //客户生产记录首行行号
+
+            $sql="select projectname,part,(Grade+TSName) grade,btrans,quality,remark1 from MSaleDay where pdate='".$pdate."' and CustName='".$val['custname']."'"; //查询当前客户的生产记录
+
+            $rows = Db::query($sql);
+            foreach ($rows as $key => $val) {//循环遍历客户的生产记录
+                // var_dump($val['part']);
+
+                $objSheet->setCellValue('C'.$index, $val['projectname']);
+                // $objSheet->getStyle('C'.$index)->getAlignment()->setWrapText(true);
+                $objSheet->setCellValue('D'.$index, $val['part']);
+                $objSheet->setCellValue('E'.$index, $val['grade']);
+                $objSheet->setCellValue('F'.$index, $val['btrans']);
+                $objSheet->setCellValue('G'.$index, $val['quality']);
+                //$objSheet->getStyle('G'.$index)->getNumberFormat()->setFormatCode('#,##0.0');//格式化数字
+                $objSheet->getStyle('G'.$index)->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_RIGHT); //设置方量列数值右对齐
+                $objSheet->setCellValue('H'.$index, $val['remark1']);
+                $index++; //行号自增
+            }
+        $num++;//序号自增
+        $objSheet->mergeCells('B'.$old_index.':B'.($index-1)); //合并客户名称单元格
+        $objSheet->getStyle('B'.$old_index)->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_TOP);//设置客户名称单元格上对齐
+        $objSheet->mergeCells('A'.$old_index.':A'.($index-1)); //合并序号单元格
+        $objSheet->getStyle('A'.$old_index)->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_TOP);//设置序号单元格上对齐
+        //var_dump('B'.$old_index.':B'.$index);
+            
+        }
+
+        
+       
+
+        //表格添加边框
+        $classBorderStyle = self::getBorderStyle('333333'); //设定边框颜色
+        $objSheet->getStyle("A4:H".$index)->applyFromArray($classBorderStyle);//给表格加上边框
+        
+        //合计行导出
+        $objSheet->mergeCells('B'.$index.':F'.$index); //合并合计单元格
+        $objSheet->setCellValue('B'.$index, '方量合计：');
+        $objSheet->setCellValue('G'.$index, '=SUM(G5:G'.($index-1).')');//方量合计数
+
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel5'); //生成Excel文件
+        // $objWriter->save("D:/export_1.xls");//保存文件
+
+        self::browser_export('Excel5', "browser_excel03.xls");//输出文件到浏览器
+        $objWriter->save('php://output');
+
+    }
+
+
 
     /**
     **
@@ -127,18 +226,11 @@ class Msaleday extends Controller
     function getBorderStyle($color){
         $styleArray = array(
             'borders' => array(
-                'outline' => array(
-                    'style' => \PHPExcel_Style_Border::BORDER_THICK,
+                'allborders' => array(
+                    'style' => \PHPExcel_Style_Border::BORDER_THIN,
                     'color' => array('argb' => $color),
                  ),
-                'vertical' => array(
-                    'style' => \PHPExcel_Style_Border::BORDER_THICK,
-                    'color' => array('argb' => $color),
-                 ),
-                'horizontal' => array(
-                    'style' => \PHPExcel_Style_Border::BORDER_THICK,
-                    'color' => array('argb' => $color),
-                 ),
+                
             ),
         );
         return $styleArray;
